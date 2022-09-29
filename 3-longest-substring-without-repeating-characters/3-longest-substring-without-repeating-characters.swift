77a739b0c36c1bc8a44de58832081b106bf62870
КолумбиЯ <gloomikon@gmail.com>
2022-09-29 23:07:44 +0200

    Time: 26 ms (73.79%), Space: 14.5 MB (78.70%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        guard !s.isEmpty else {
+            return 0
+        }
+
+        var len = 0
+        var chars = [Character]()
+
+        for char in s {
+
+            if let idx = chars.firstIndex(of: char) {
+                chars.removeSubrange(0...idx)
+            }
+
+            chars.append(char)
+            len = max(len, chars.count)
+        }
+
+        return len
+    }
+}
