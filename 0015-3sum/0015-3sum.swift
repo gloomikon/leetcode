cbcfaab4d7fa3c23d24282bdf49a7e66894e284d
gloomikon <gloomikon@gmail.com>
2023-07-24 10:31:11 +0200

    Time: 228 ms (55.69%), Space: 19 MB (7.69%) - LeetHub

--- 0015-3sum/0015-3sum.swift
+++ 0015-3sum/0015-3sum.swift
@@ -1,33 +1,30 @@
 class Solution {
-    
     func threeSum(_ nums: [Int]) -> [[Int]] {
-
         var answer: Set<[Int]> = []
-
         let size = nums.count
-
-        var map: [Int: [Int]] = nums.enumerated().reduce([:]) { dict, tuple in
-            let index = tuple.offset
-            let element = tuple.element
-            var dict = dict
-            if let array = dict[element] {
-                dict[element] = array + [index]
-            } else {
-                dict[element] = [index]
-            }
-            return dict
-        }
+        let sortedNums = nums.sorted()
 
         for i in 0..<size {
-            for j in i + 1..<size {
-                let a = nums[i]
-                let b = nums[j]
-                let sum = nums[i] + nums[j]
-                let c = -sum
-                if let array = map[c],
-                   array.contains(where: { $0 != i && $0 != j }) {
-                    let newMember = [a, b, c].sorted()
-                    answer.insert(newMember)
+//            if i > 0 && sortedNums[i] == sortedNums[i - 1] {
+//                continue // Skip duplicate elements to avoid duplicate triplets
+//            }
+
+            var left = i + 1
+            var right = size - 1
+
+            while left < right {
+                let sum = sortedNums[i] + sortedNums[left] + sortedNums[right]
+
+                if sum == 0 {
+                    answer.insert([sortedNums[i], sortedNums[left], sortedNums[right]])
+
+                    // Skip duplicate elements to avoid duplicate triplets
+                    repeat { left += 1 } while left < right && sortedNums[left] == sortedNums[left - 1]
+                    repeat { right -= 1 } while left < right && sortedNums[right] == sortedNums[right + 1]
+                } else if sum < 0 {
+                    left += 1
+                } else {
+                    right -= 1
                 }
             }
         }
