4ad26ad4d50429480f4ad30d4e9ba45eb5a45d39
gloomikon <gloomikon@gmail.com>
2023-07-25 08:47:25 +0200

    Time: 839 ms (83.93%), Space: 17.9 MB (71.43%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.swift
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.swift
@@ -1,18 +1,9 @@
-extension Array {
-    subscript(safe index: Int) -> Element? {
-        indices ~= index ? self[index] : nil
-    }
-}
-
 class Solution {
     func peakIndexInMountainArray(_ arr: [Int]) -> Int {
-        for i in 0..<arr.count {
-            if let a = arr[safe: i - 1], let c = arr[safe: i + 1] {
-                if a < arr[i], arr[i] > c {
-                    return i
-                }
-            }
+        var i = 0
+        while arr[i] < arr[i + 1] {
+            i += 1
         }
-        return 0
+        return i
     }
 }
