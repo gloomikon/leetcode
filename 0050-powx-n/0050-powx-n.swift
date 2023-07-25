b0f560bec193a4f8571e55f7a77dd4b806a7869f
gloomikon <gloomikon@gmail.com>
2023-07-25 08:24:01 +0200

    Time: 0 ms (100.00%), Space: 13.9 MB (79.82%) - LeetHub

--- 0050-powx-n/0050-powx-n.swift
+++ 0050-powx-n/0050-powx-n.swift
@@ -1,5 +1,10 @@
 class Solution {
     func myPow(_ x: Double, _ n: Int) -> Double {
-        pow(x, Double(n))
+        if n == 0 { return 1 }
+        if n < 0 { return 1 / myPow(x, -n) }
+        
+        return n % 2 == 0 ?
+        myPow(x * x, n / 2) :
+        x * myPow(x * x, n / 2)
     }
 }
