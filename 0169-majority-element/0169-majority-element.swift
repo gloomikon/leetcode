541c228e0eb6cbc500fef4291b025fafc9a2514a
gloomikon <gloomikon@gmail.com>
2023-07-26 09:41:37 +0200

    Time: 103 ms (67.92%), Space: 15.7 MB (59.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.swift
+++ 0169-majority-element/0169-majority-element.swift
@@ -1,3 +1,4 @@
+// https://www.cantorsparadise.com/computing-the-majority-vote-on-pen-and-paper-the-boyer-moore-algorithm-3f739de3d8cb
 class Solution {
     func majorityElement(_ nums: [Int]) -> Int {
         var candidate = 0
