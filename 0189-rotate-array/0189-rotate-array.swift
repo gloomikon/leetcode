dcebcf5f1744668f0c48620e003f2746cfd439a7
gloomikon <gloomikon@gmail.com>
2023-07-26 10:06:08 +0200

    Time: 229 ms (97.29%), Space: 21.5 MB (31.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.swift
+++ 0189-rotate-array/0189-rotate-array.swift
@@ -1,7 +1,29 @@
 class Solution {
     func rotate(_ nums: inout [Int], _ k: Int) {
-        for _ in 0..<k {
-            nums = [nums.popLast()!] + nums
+        
+        let n = nums.count
+
+        // Modulo ensures the range within the length and avoids unnecessary rotation.
+        let k = k % n
+
+        reverse(&nums, 0, n-1)
+        reverse(&nums, 0, k-1)
+        reverse(&nums, k, n-1)
+    }
+
+    func reverse(_ nums: inout [Int], _ start: Int, _ end: Int) {
+
+        var start = start
+        var end = end
+
+        while start < end{
+
+            let temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+
+            start += 1
+            end -= 1
         }
     }
 }
