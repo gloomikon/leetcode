8d25e8c83986f6d4e6e5264cc6f442343f9f043b
gloomikon <gloomikon@gmail.com>
2023-07-26 08:41:56 +0200

    Time: 1255 ms (80.00%), Space: 18.2 MB (40.00%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.swift
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.swift
@@ -6,8 +6,8 @@ class Solution {
 
         while left <= right {
             let targetSpeed = (left + right) / 2
+            
             // Calculate time for this speed
-
             var totalTime: Double = 0
             dist.enumerated().forEach { index, elem in
                 let currentDistanceTime = Double(elem) / Double(targetSpeed)
