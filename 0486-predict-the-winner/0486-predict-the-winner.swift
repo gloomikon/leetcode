235325aeb871f2e7c17cab563685c41025b4afac
gloomikon <gloomikon@gmail.com>
2023-07-28 11:30:26 +0200

    Time: 2 ms (80.00%), Space: 14 MB (60.00%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.swift
+++ 0486-predict-the-winner/0486-predict-the-winner.swift
@@ -1,71 +1,14 @@
 class Solution {
+    func PredictTheWinner(_ nums: [Int]) -> Bool {
+        var (dp, n) = (nums, nums.count)
 
-    private enum Player {
-        case first
-        case second
-    }
-
-    private func takeNumber(
-        from array: [Int],
-        firstPlayerSum: Int,
-        secondPlayerSum: Int,
-        playerToTake player: Player
-    ) -> Bool {
-        // No elements to take
-        if array.isEmpty { return firstPlayerSum >= secondPlayerSum }
-
-        let count = array.count
-
-        // Only one element to take, only one possible take
-        if count == 1 {
-            switch player {
-            case .first:
-                return takeNumber(
-                    from: [],
-                    firstPlayerSum: firstPlayerSum + array[0],
-                    secondPlayerSum: secondPlayerSum,
-                    playerToTake: .second
-                )
-            case .second:
-                return takeNumber(
-                    from: [],
-                    firstPlayerSum: firstPlayerSum,
-                    secondPlayerSum: secondPlayerSum + array[0],
-                    playerToTake: .first
-                )
+        for diff in 1..<n {
+            for left in 0..<n - diff {
+                let right = left + diff
+                dp[left] = max(nums[left] - dp[left + 1], nums[right] - dp[left])
             }
         }
 
-        // Two or more elememts, can take from the beginning or from the end
-        switch player {
-        case .first:
-            return takeNumber(
-                from: Array(array[1...]),
-                firstPlayerSum: firstPlayerSum + array[0],
-                secondPlayerSum: secondPlayerSum,
-                playerToTake: .second
-            ) || takeNumber(
-                from: Array(array[0..<count - 1]),
-                firstPlayerSum: firstPlayerSum + array[count - 1],
-                secondPlayerSum: secondPlayerSum,
-                playerToTake: .second
-            )
-        case .second:
-            return takeNumber(
-                from: Array(array[1...]),
-                firstPlayerSum: firstPlayerSum,
-                secondPlayerSum: secondPlayerSum + array[0],
-                playerToTake: .first
-            ) && takeNumber(
-                from: Array(array[0..<count - 1]),
-                firstPlayerSum: firstPlayerSum,
-                secondPlayerSum: secondPlayerSum + array[count - 1],
-                playerToTake: .first
-            )
-        }
-    }
-
-    func PredictTheWinner(_ nums: [Int]) -> Bool {
-        takeNumber(from: nums, firstPlayerSum: 0, secondPlayerSum: 0, playerToTake: .first)
+        return dp[0] >= 0
     }
 }
