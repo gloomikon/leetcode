6b439482665c8ce4b6ff046cdcacb8a0df82e0c0
gloomikon <gloomikon@gmail.com>
2023-07-29 15:11:41 +0200

    Time: 14 ms (64.97%), Space: 14.4 MB (36.10%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.swift
@@ -0,0 +1,21 @@
+class Solution {
+
+    private var dp: [Int: [String]] = [0: [""]]
+
+    func generateParenthesis(_ n: Int) -> [String] {
+        if let parentheses = dp[n] {
+            return parentheses
+        }
+        var result: [String] = []
+
+        for i in 0..<n {
+            for l in generateParenthesis(i) {
+                for r in generateParenthesis(n - 1 - i) {
+                    result.append("(" + l + ")" + r)
+                }
+            }
+        }
+        dp[n] = result
+        return result
+    }
+}
