06df322de373eb85ce83a2f71a23c0e4a48a95b8
gloomikon <gloomikon@gmail.com>
2023-07-29 12:45:28 +0200

    Time: 80 ms (33.33%), Space: 15.4 MB (66.67%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.swift
@@ -0,0 +1,32 @@
+class Solution {
+
+    private struct DP: Hashable {
+        let a: Int
+        let b: Int
+    }
+
+    func soupServings(_ n: Int) -> Double {
+        var dp: [DP: Double] = [:]
+        let m = Int(ceil(Double(n) / 25))
+        if m == 0 { return 0.5 }
+
+        func serve(_ a: Int, _ b: Int) -> Double {
+            if a <= 0 && b <= 0 { return 0.5 }
+            if a <= 0 { return 1 }
+            if b <= 0 { return 0 }
+            let DP = DP(a: a, b: b)
+            if let probability = dp[DP] { return probability }
+            let probability = (serve(a - 4, b) + serve(a - 3, b - 1) + serve(a - 2, b - 2) + serve(a - 1, b - 3)) / 4
+            dp[DP] = probability
+            return probability
+        }
+
+        for k in 1...m {
+            if serve(k, k) > 1 - 1e-5 {
+                return 1
+            }
+        }
+
+        return serve(m, m)
+    }
+}
