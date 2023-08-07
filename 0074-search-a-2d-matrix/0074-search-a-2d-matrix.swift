0dfa803fbd718d2ac849cb454eecdfcf8b5b8696
gloomikon <gloomikon@gmail.com>
2023-08-07 17:38:20 +0200

    Time: 22 ms (63.81%), Space: 13.9 MB (90.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
         let array = matrix.flatMap { $0 }
         guard !array.isEmpty else { return false }
@@ -16,5 +17,36 @@ class Solution {
         }
 
         return array[left] == target
+    }
+     */
+
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        var (top, bottom) = (0, matrix.count - 1)
+        while top < bottom {
+            let middle = (top + bottom) / 2
+            let row = matrix[middle]
+            let (min, max) = (row.first!, row.last!)
+            if min...max ~= target { break }
+            if target < min {
+                bottom = middle - 1
+            } else {
+                top = middle + 1
+            }
+        }
+
+        let row = matrix[(top + bottom) / 2]
+        var (left, right) = (0, row.count - 1)
+        while left < right {
+            let middle = (left + right) / 2
+            let b = row[middle]
+            if b == target { return true }
+            if b > target {
+                right = middle - 1
+            } else {
+                left = middle + 1
+            }
+        }
+
+        return row[left] == target
     }
 }
