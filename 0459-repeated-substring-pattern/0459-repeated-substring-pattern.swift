40111654ffbc5f51d71299dd6518979ccb309d50
gloomikon <gloomikon@gmail.com>
2023-08-21 21:56:38 +0200

    Time: 131 ms (61.76%), Space: 14.4 MB (44.12%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.swift
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.swift
@@ -1,12 +1,11 @@
 class Solution {
     func repeatedSubstringPattern(_ s: String) -> Bool {
         let a = Array(s)
-        for i in 0..<a.count / 2 {
-            guard a.count % (i + 1) == 0 else { continue }
-            let substring = String(a[0...i])
-            if s.components(separatedBy: substring)
-                .compactMap({ $0.isEmpty ? nil : $0 })
-                .isEmpty {
+        guard a.count > 1 else { return false }
+        for i in 1...a.count / 2 {
+            guard a.count % i == 0 else { continue }
+            let pattern = String(repeating: String(a[0..<i]), count: a.count / i)
+            if pattern == s {
                 return true
             }
         }
