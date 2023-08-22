2bee588bbf518e265cec9fe6693b43450bdc277b
gloomikon <gloomikon@gmail.com>
2023-08-22 10:33:52 +0200

    Time: 126 ms (61.76%), Space: 14.1 MB (61.76%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.swift
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.swift
@@ -1,18 +1,18 @@
 class Solution {
-//    func repeatedSubstringPattern(_ s: String) -> Bool {
-//        let a = Array(s)
-//        guard a.count > 1 else { return false }
-//        for i in 1...a.count / 2 {
-//            guard a.count % i == 0 else { continue }
-//            let pattern = String(repeating: String(a[0..<i]), count: a.count / i)
-//            if pattern == s {
-//                return true
-//            }
-//        }
-//        return false
-//    }
+   func repeatedSubstringPattern(_ s: String) -> Bool {
+       let a = Array(s)
+       guard a.count > 1 else { return false }
+       for i in 1...a.count / 2 {
+           guard a.count % i == 0 else { continue }
+           let pattern = String(repeating: String(a[0..<i]), count: a.count / i)
+           if pattern == s {
+               return true
+           }
+       }
+       return false
+   }
 
-    func repeatedSubstringPattern(_ s: String) -> Bool {
-        String(Array(s + s)[1...].dropLast()).contains(s)
-    }
+    // func repeatedSubstringPattern(_ s: String) -> Bool {
+    //     String(Array(s + s)[1...].dropLast()).contains(s)
+    // }
 }
